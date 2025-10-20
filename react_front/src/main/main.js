43e8aa3d83491709248f8090e2b41aec99275f9c
federico cuervo duran <fedecuervo44@hotmail.com>
2025-10-20 17:29:34 -0500

    Commit vs 6 20/10/2025

--- react_front/src/main/main.js
+++ react_front/src/main/main.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useState, useEffect} from "react"
 import { BuiltButton } from "./builtButton.js";
 
-const urlApi = process.env.API_NODE || "http://localhost:8000"
+const urlApi = process.env.REACT_APP_API_NODE || "http://localhost:8000"
 const URI = `${ urlApi }/valUser`;
 
 const Main = () => {
