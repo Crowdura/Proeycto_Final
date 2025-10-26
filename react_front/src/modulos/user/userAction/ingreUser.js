0f792bef6909413769efa8b5e9a020930d7ef40b
federico cuervo duran <fedecuervo44@hotmail.com>
2025-10-26 15:56:04 -0500

    Commit vs4 26/10/2025

--- react_front/src/modulos/user/userAction/ingreUser.js
+++ react_front/src/modulos/user/userAction/ingreUser.js
@@ -29,7 +29,7 @@ const IngreUser = () => {
     }
     const valIngre = async () => {
         const req = await Ingreuser()
-        
+        console.log(req)
         setTimeout(() => {
             navigate('/')
             window.location.reload()
@@ -71,7 +71,7 @@ const IngreUser = () => {
                                             </div>
 
                                             <div className="mb-12 pb-1 pt-1 text-center">
-                                                <button onClick={ (e) => valIngre } id="loginIngre" className="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-dark-3 transition duration-150 ease-in-out hover:shadow-dark-2 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:shadow-dark-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong" type="button" style={{background: "linear-gradient(to right, rgb(2 6 23 / var(--tw-bg-opacity)), rgb(2 6 23 / var(--tw-bg-opacity)), #071c35, #051e3b)"}}>
+                                                <button onClick={ (e) => valIngre() } id="loginIngre" className="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-dark-3 transition duration-150 ease-in-out hover:shadow-dark-2 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:shadow-dark-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong" type="button" style={{background: "linear-gradient(to right, rgb(2 6 23 / var(--tw-bg-opacity)), rgb(2 6 23 / var(--tw-bg-opacity)), #071c35, #051e3b)"}}>
                                                     log in
                                                 </button>
                                                 <a href="">Se le olvido la contraseña?</a>
